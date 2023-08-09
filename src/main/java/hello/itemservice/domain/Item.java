53808401363e75abbab6e758b3b612ef5ff37442
juhoon212 <ws5501@naver.com>
2023-08-10 01:28:05 +0900

    transfer to JPA

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,16 +2,23 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+//@Table(name = "item") => 객체명이랑 같으면 생략 가능
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
+
     private Integer price;
     private Integer quantity;
 
-    public Item() {
+    public Item() { // JPA는 기본 생성자가 있어야 한다.
     }
 
     public Item(String itemName, Integer price, Integer quantity) {
